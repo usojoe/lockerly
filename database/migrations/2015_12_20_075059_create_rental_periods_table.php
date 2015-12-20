3b6cf04e43e6d7556b9f13360f18386f2d7a081e
Joe Lee <josephandrewlee@g.harvard.edu>
2015-12-20 03:37:58 -0500

    added models

--- database/migrations/2015_12_20_075059_create_rental_periods_table.php
+++ database/migrations/2015_12_20_075059_create_rental_periods_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBooktofromTable extends Migration
+class CreateRentalPeriodsTable extends Migration
 {
     /**
      * Run the migrations.
